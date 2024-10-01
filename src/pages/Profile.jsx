79ab599456c22fe54d739472941ce767ecef5e5b
clod44 <Rt_Sayochi@hotmail.com>
2024-10-01 18:33:34 +0300

    accounts completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
-import { Divider } from '@nextui-org/react';
+import { Divider, Tabs, Tab, Avatar, Button, Card, CardBody, CardFooter, } from '@nextui-org/react';
 
 export default function Profile() {
     const [isAnonymous, setIsAnonymous] = useState(false);
@@ -35,30 +35,37 @@ export default function Profile() {
         <>
             {isAnonymous && (
                 <div className='px-2 bg-black'>
-                    <p className='text-xs w-full text-center hover:text-lg duration-200 text-red-500 font-bold'>You are logged in as Anonymous. If you log out, you will lose access to this account forever. Feel free to test the application anonymously.</p>
+                    <p className='text-xs w-full text-center hover:text-lg duration-200 text-danger font-bold'>You are logged in as Anonymous. If you log out, you will lose access to this account forever. Feel free to test the application anonymously.</p>
                     <Divider />
                 </div>
             )}
-            <div className="flex flex-col flex-grow overflow-y-auto p-4">
-                <h1 className="text-2xl font-bold">Profile</h1>
-                {user ? (
-                    <div className="mt-4">
-                        <h2 className="text-xl">User Details</h2>
-                        <p><strong>Email:</strong> {user.email}</p>
-                        <p><strong>UID:</strong> {user.uid}</p>
-                        <p><strong>Display Name:</strong> {user.displayName || 'Not set'}</p>
-                        <p><strong>Photo URL:</strong> {user.photoURL || 'Not set'}</p>
-                        <button
-                            onClick={handleLogout}
-                            className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
-                        >
-                            Logout
-                        </button>
-                    </div>
-                ) : (
-                    <p>No user is currently logged in.</p>
+            <div className="flex flex-col flex-grow overflow-y-auto p-4 items-center">
+                <Tabs centered variant='underlined'>
+                    <Tab key="profile" title="Profile">
+                        <Card className='bg-background shadow-lg border border-default'>
+                            <CardBody>
+                                <div className='p-4 flex gap-5 items-center flex-wrap justify-center mb-4'>
+                                    <Avatar src={user?.photoURL} className="w-40 h-40 text-large" isBordered color='primary' />
+                                    <div className='flex-grow'>
+                                        <p><strong>Name:</strong> {user?.displayName || 'Not set'}</p>
+                                        <p><strong>Email:</strong> {user?.email}</p>
+                                    </div>
+                                </div>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                                <Button className='w-full' size='sm' variant="flat" color="danger" onClick={handleLogout}>Logout</Button>
+                            </CardFooter>
 
-                )}
+                        </Card>
+                    </Tab>
+                    <Tab key="settings" title="Settings">
+                        <div>
+                            No settings
+                        </div>
+                    </Tab>
+                </Tabs>
+
             </div >
         </>
     );
